9eab8213672837f505b2f999819d0b5f555e0bae
prudhvivadla <prudhvic4@gmail.com>
2022-12-27 06:17:28 -0500

    project setup commit version 0.0

---
+++ src/test/java/learn/design/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.design.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
